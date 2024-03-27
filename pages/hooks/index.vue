b1a36db6447918be49a04dc609dab0edc300f016
mezoo <mo3tz3adel@gmail.com>
2024-03-27 15:59:28 +0200

    use onMountd hook

---
+++ pages/hooks/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+const nuxt = useNuxtApp();
+nuxt.hook('app:ready', function () {
+    console.log("hello from the %c mounted hook", "color:blue");
+})
+
+onMounted(() => {
+    console.log("hello from the %c mounted onMounted", "color:blue");
+    
+});
+</script>
+
+<style lang="scss" scoped></style>
